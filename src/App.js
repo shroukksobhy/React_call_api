fb35ff817223bd3744989c1f2301a4e76440e2b2
shroukksobhy <shroukksobhy@gmail.com>
2021-12-07 21:21:24 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios';
 function App() {
+  axios.post(`https://banquemisr.gateway.mastercard.com/api/rest/version/60/merchant/TESTMERCHTST_EGP/session`, {}, {
+    auth: {
+      username: 'merchant.TESTMERCHTST_EGP',
+      password: '26c176246ea2389bea43649c5e1d426e'
+    }
+  })
+    .then(res => {
+
+      console.log(res.data)
+    })
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Editt <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
